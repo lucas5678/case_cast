86d1159f7a1c398a3b467577cdbd4311093b24c3
Lucas Alves <109555780+lucas5678@users.noreply.github.com>
2026-03-28 16:50:29 -0300

    feat: adicionar log de tentativas de login em ContaLogin

--- src/main/java/com/teste/banco/banco/Services/ContaLogin.java
+++ src/main/java/com/teste/banco/banco/Services/ContaLogin.java
@@ -1,5 +1,7 @@
 package com.teste.banco.banco.Services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.teste.banco.banco.DTO.ModelLoginDTO;
@@ -10,6 +12,8 @@ import com.teste.banco.banco.Repository.UsuariosRepository;
 
 @Service
 public class ContaLogin {
+    private static final Logger log = LoggerFactory.getLogger(ContaLogin.class);
+    
     @Autowired
     UsuariosRepository usuariosRepository;
 
@@ -17,24 +21,34 @@ public class ContaLogin {
     ContaRepository contaRepository;
 
     public ModelLoginDTO logar(ModelLoginDTO usuario) {
-        ModelLoginDTO usuarioBanco = new ModelLoginDTO();
-        // verifica se usuarios e senha existem no banco de dados
-        ModelUsuarios usuarioBancoEntity = usuariosRepository.findByCpfAndSenha(usuario.getCpf(), usuario.getSenha())
-                .orElseThrow(() -> new RuntimeException("Usuário ou senha inválidos!"));
-        if ("user".equals(usuarioBancoEntity.getPerfil())) {
-            if (!usuarioBancoEntity.getCpf().equals("")) {
-                ModelConta contaBancoEntity = contaRepository.findByCpf(usuarioBancoEntity.getCpf())
-                        .orElseThrow(() -> new RuntimeException("Conta não encontrada!"));
-                usuarioBanco.setNumeroConta(contaBancoEntity.getNumeroConta());
-                usuarioBanco.setTitular(usuarioBancoEntity.getNome());
-                usuarioBanco.setSaldo(contaBancoEntity.getSaldo());
+        log.info("Tentativa de login para CPF: {}", usuario.getCpf());
+        
+        try {
+            ModelLoginDTO usuarioBanco = new ModelLoginDTO();
+            // verifica se usuarios e senha existem no banco de dados
+            ModelUsuarios usuarioBancoEntity = usuariosRepository.findByCpfAndSenha(usuario.getCpf(), usuario.getSenha())
+                    .orElseThrow(() -> new RuntimeException("Usuário ou senha inválidos!"));
+            
+            log.info("Login bem-sucedido para CPF: {} | Perfil: {}", usuarioBancoEntity.getCpf(), usuarioBancoEntity.getPerfil());
+            
+            if ("user".equals(usuarioBancoEntity.getPerfil())) {
+                if (!usuarioBancoEntity.getCpf().equals("")) {
+                    ModelConta contaBancoEntity = contaRepository.findByCpf(usuarioBancoEntity.getCpf())
+                            .orElseThrow(() -> new RuntimeException("Conta não encontrada!"));
+                    usuarioBanco.setNumeroConta(contaBancoEntity.getNumeroConta());
+                    usuarioBanco.setTitular(usuarioBancoEntity.getNome());
+                    usuarioBanco.setSaldo(contaBancoEntity.getSaldo());
+                }
             }
+            usuarioBanco.setCpf(usuarioBancoEntity.getCpf());
+            usuarioBanco.setSenha(usuarioBancoEntity.getSenha());
+            usuarioBanco.setPerfil(usuarioBancoEntity.getPerfil());
+            usuarioBanco.setNome(usuarioBancoEntity.getNome());
+            return usuarioBanco;
+        } catch (RuntimeException e) {
+            log.warn("Falha no login para CPF: {} | Motivo: {}", usuario.getCpf(), e.getMessage());
+            throw e;
         }
-        usuarioBanco.setCpf(usuarioBancoEntity.getCpf());
-        usuarioBanco.setSenha(usuarioBancoEntity.getSenha());
-        usuarioBanco.setPerfil(usuarioBancoEntity.getPerfil());
-        usuarioBanco.setNome(usuarioBancoEntity.getNome());
-        return usuarioBanco;
     }
 
     public ModelLoginDTO AtualizaInformacoes(ModelLoginDTO usuario) {
