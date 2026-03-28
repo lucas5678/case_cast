29a3edc8b9fa297df56c31e43d0d5489b11d579e
Lucas Alves <109555780+lucas5678@users.noreply.github.com>
2026-03-28 17:20:38 -0300

    feat: adiciona LoginException para erros de autenticação

---
+++ src/main/java/com/teste/banco/banco/exceptions/LoginException.java
@@ -0,0 +1,13 @@
+package com.teste.banco.banco.exceptions;
+
+public class LoginException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public LoginException(String message) {
+        super(message);
+    }
+
+    public LoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
