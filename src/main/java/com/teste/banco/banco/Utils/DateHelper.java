bc5e3added6bfbaf6b20f313f05434f0541a388c
Lucas Alves <109555780+lucas5678@users.noreply.github.com>
2026-04-16 16:49:29 -0300

    feat: implement card VQr5Fq3K7Sad1QPl1z2f - Teste de integração do Claudinho

--- src/main/java/com/teste/banco/banco/Utils/DateHelper.java
+++ src/main/java/com/teste/banco/banco/Utils/DateHelper.java
@@ -3,10 +3,18 @@ package com.teste.banco.banco.Utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DateHelper {
 
-    public static String getDataAtualFormatada() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        return LocalDateTime.now().format(formatter);
+    private static final String DEFAULT_DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
+    public String getCurrentDateFormatted() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT));
+    }
+
+    public String getCurrentDateFormatted(String pattern) {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern));
     }
 }
